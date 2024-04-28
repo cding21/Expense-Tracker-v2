0d87c32f668bc32d9c5f62711390f2ad8f0c0eea
Charlie <64823818+cding21@users.noreply.github.com>
2024-04-28 22:24:28 +1000

    feat: Server call logging

--- src/backend/src/main/kotlin/au/com/cding21/plugins/HTTP.kt
+++ src/backend/src/main/kotlin/au/com/cding21/plugins/HTTP.kt
@@ -2,11 +2,11 @@ package au.com.cding21.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.request.*
+import org.slf4j.event.Level
 
 fun Application.configureHTTP() {
     install(DefaultHeaders) {
@@ -20,5 +20,17 @@ fun Application.configureHTTP() {
         allowHeader(HttpHeaders.Authorization)
         allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call ->
+            call.request.path().startsWith("/api/v1")
+        }
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+        }
     }
 }
