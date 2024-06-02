0f442913d4eeb7792aca6c8fcac563e8c5a7d9fa
Thanakon <kimsanthanakon@gmail.com>
2024-06-02 16:16:06 +1000

    feat: update landing page branding and description

--- src/frontend/components/Welcome/Welcome.tsx
+++ src/frontend/components/Welcome/Welcome.tsx
@@ -6,17 +6,25 @@ export function Welcome() {
     <>
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+        <Text
+          inherit
+          variant="gradient"
+          component="span"
+          gradient={{ from: 'lightblue', to: 'blue' }}
+        >
+          Expense Tracker
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
+        This starter Next.js project is a minimal setup for Charlie&apos;s Ultimate Expense Tracker
+        v2.{' '}
+        <Anchor
+          href="https://github.com/cding21/Expense-Tracker-v2/compare/frontend-starter?diff=split&w="
+          size="lg"
+        >
+          Click here
+        </Anchor>{' '}
+        to get see the project source code.
       </Text>
     </>
   );
