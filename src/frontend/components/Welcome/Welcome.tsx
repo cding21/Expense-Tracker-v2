7b7c350e8287409442c83d51db04bf41474fd9cf
cding21 <charlieding21@gmail.com>
2024-06-09 22:27:41 +1000

    fix: source code link

--- src/frontend/components/Welcome/Welcome.tsx
+++ src/frontend/components/Welcome/Welcome.tsx
@@ -19,7 +19,7 @@ export function Welcome() {
         This starter Next.js project is a minimal setup for Charlie&apos;s Ultimate Expense Tracker
         v2.{' '}
         <Anchor
-          href="https://github.com/cding21/Expense-Tracker-v2/compare/frontend-starter?diff=split&w="
+          href="https://github.com/cding21/Expense-Tracker-v2"
           size="lg"
         >
           Click here
