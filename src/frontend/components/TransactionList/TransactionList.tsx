d6fe2b53d301ced48790fecd198d51d6b36b1ebd
cding21 <charlieding21@gmail.com>
2024-06-10 01:57:41 +1000

    feat: additional clickable items
    also tested basic integration with backend, will implement further once authorisation is better implemented

--- src/frontend/components/TransactionList/TransactionList.tsx
+++ src/frontend/components/TransactionList/TransactionList.tsx
@@ -1,8 +1,26 @@
+// 'use client';
+
 import { Stack } from '@mantine/core';
 import { mockTransactionList } from '@/mockTransaction';
 import { TransactionListItem } from './TransactionListItem';
+// import { useEffect, useState } from 'react';
 
 export function TransactionList() {
+  // TODO: Once integration with the backend is complete, uncomment the following code:
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const token = sessionStorage.getItem('token');
+  //   fetch('/api/v1/transactions', {
+  //     headers: {
+  //     Authorization: `Bearer ${token}`
+  //     }
+  //   })
+  //     .then((response) => response.json())
+  //     .then((responseData) => setData(responseData))
+  //     .catch((error) => console.error(error));
+  // }, []);
+
   const data = mockTransactionList;
 
   return (
