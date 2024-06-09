553f122fb58e8547897d9bef0d6180732665c0ad
cding21 <charlieding21@gmail.com>
2024-06-10 01:21:09 +1000

    fix: welcome test fixed to address new link/text

--- src/frontend/components/Welcome/Welcome.test.tsx
+++ src/frontend/components/Welcome/Welcome.test.tsx
@@ -4,9 +4,9 @@ import { Welcome } from './Welcome';
 describe('Welcome component', () => {
   it('has correct Next.js theming section link', () => {
     render(<Welcome />);
-    expect(screen.getByText('this guide')).toHaveAttribute(
+    expect(screen.getByText('Click here')).toHaveAttribute(
       'href',
-      'https://mantine.dev/guides/next/'
+      'https://github.com/cding21/Expense-Tracker-v2'
     );
   });
 });
