4acff1b9bed8f9297323c80cb20afde3cbcb83a9
cding21 <charlieding21@gmail.com>
2024-06-13 02:09:39 +1000

    feat: additional transaction tests

--- src/backend/src/test/kotlin/au/com/cding21/data/TransactionTest.kt
+++ src/backend/src/test/kotlin/au/com/cding21/data/TransactionTest.kt
@@ -1,17 +1,77 @@
 package au.com.cding21.data
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertEquals
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import kotlin.test.Test
+import org.bson.Document
+
 
 class TransactionTest {
-    // Write a Junit test for the Transaction class
-    // Test the toDocument method
+    @Test
+    fun testToDocument() {
+        val transaction = Transaction(
+            "testUser",
+            LocalDate.parse("01/01/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+            -100.0,
+            "description",
+            "category",
+            "fromAccount",
+            "",
+            "toAccount",
+            ""
+        )
+        val expectedDocument = Document(
+            mapOf(
+                "userId" to "testUser",
+                "date" to "01/01/2022",
+                "amount" to -100.0,
+                "description" to "description",
+                "category" to "category",
+                "fromAccount" to "fromAccount",
+                "fromNote" to "",
+                "toAccount" to "toAccount",
+                "toNote" to ""
+            )
+        )
 
-    // Test the fromDocument method
+        val actualDocument = transaction.toDocument()
 
-    // Test the fromCsvLine method
+        assertEquals(expectedDocument, actualDocument)
+    }
+
+    @Test
+    fun testFromDocument() {
+        val document = Document(
+            mapOf(
+                "userId" to "testUser",
+                "date" to "01/01/2022",
+                "amount" to -100.0,
+                "description" to "description",
+                "category" to "category",
+                "fromAccount" to "fromAccount",
+                "fromNote" to "",
+                "toAccount" to "toAccount",
+                "toNote" to ""
+            )
+        )
+        val expectedTransaction = Transaction(
+            "testUser",
+            LocalDate.parse("01/01/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+            -100.0,
+            "description",
+            "category",
+            "fromAccount",
+            "",
+            "toAccount",
+            ""
+        )
+
+        val actualTransaction = Transaction.fromDocument(document)
+
+        assertEquals(expectedTransaction, actualTransaction)
+    }
+
     @Test
     fun testFromCsvLine() {
         val csvLine = "01/01/2022,\"-100.0\",description,category,fromAccount,,toAccount,"
