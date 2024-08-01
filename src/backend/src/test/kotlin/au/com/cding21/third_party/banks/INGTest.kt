f8e822852e5c807ccd4b2ff480cc041e0969d6f2
Bowen Feng <31093400+thebowenfeng@users.noreply.github.com>
2024-08-01 11:47:42 +1000

    removed dom parsing (#38)
    Co-authored-by: Bowen Feng <bfeng@atlassian.com>

--- src/backend/src/test/kotlin/au/com/cding21/third_party/banks/INGTest.kt
+++ src/backend/src/test/kotlin/au/com/cding21/third_party/banks/INGTest.kt
@@ -1,10 +1,15 @@
 package au.com.cding21.third_party.banks
 
 import au.com.cding21.third_party.banks.util.ImageCodec
+import au.com.cding21.third_party.banks.util.throwIfNullKey
 import au.com.cding21.third_party.banks.util.waitForResponseAsync
 import au.com.cding21.third_party.banks.util.waitForSelectorAsync
 import com.microsoft.playwright.*
 import com.microsoft.playwright.options.LoadState
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -19,19 +24,14 @@ class INGTest {
             val context = browser.newContext()
             val page = context.newPage()
             page.navigate("https://www.ing.com.au/securebanking/")
-
-            try{
-                page.waitForResponse("https://www.ing.com.au/KeypadService/v1/KeypadService.svc/json/PinpadImages", Page.WaitForResponseOptions().setTimeout(10000.0)) {}
-                page.waitForResponse("https://www.ing.com.au/KeypadService/v1/KeypadService.svc/json/PinpadImages", Page.WaitForResponseOptions().setTimeout(3000.0)) {}
-            } catch (_: TimeoutError) {/* This wait is flakey sometimes */}
-            page.waitForSelector("#cifField", Page.WaitForSelectorOptions().setTimeout(5000.0))
-
-            // Use DOM parsing to emulate real client behaviour
-            val keys = page.querySelector("#keypad > div").querySelectorAll("[role=\"button\"]").filter { it.getAttribute("position") != null }
+            page.waitForResponse("https://www.ing.com.au/KeypadService/v1/KeypadService.svc/json/PinpadImages") {}
+            val response = page.waitForResponse("https://www.ing.com.au/KeypadService/v1/KeypadService.svc/json/PinpadImages") {}
+            
+            val keys = Json.decodeFromString<JsonObject>(response.text()).throwIfNullKey("KeypadImages").jsonArray
             assertEquals(keys.size, 10)
 
             for (key in keys) {
-                val imageB64 = key.querySelector("img").getAttribute("src").split(",")[1]
+                val imageB64 = key.jsonPrimitive.content
                 val image = ImageCodec.fromBase64(imageB64).crop(80, 35, 21, 35)
                 val validRefImages =  refImages.filter { it.compare(image) > 0.95 }
                 assertEquals(validRefImages.size, 1)
