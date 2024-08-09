6db0e380ab1f06ee4b058ce7f5b395e4b50474c0
cding21 <charlieding21@gmail.com>
2024-08-09 17:50:16 +1000

    feat: basic dashboard

--- src/dashboard/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/dashboard/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { FaMoon, FaSun } from "react-icons/fa";
+import { BsNintendoSwitch } from "react-icons/bs";
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
 
   return (
     <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={() => setColorScheme('light')}><FaSun /></Button>
+      <Button onClick={() => setColorScheme('dark')}><FaMoon /></Button>
+      <Button onClick={() => setColorScheme('auto')}><BsNintendoSwitch /></Button>
     </Group>
   );
 }
---
+++ src/dashboard/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import {
+  Title,
+  Text,
+  Container,
+} from '@mantine/core';
+import classes from './Dashboard.module.css';
+import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
+import { TransactionList, TransactionListProps } from '../TransactionList/TransactionList';
+import { mockTransactionList } from '@/mockTransaction';
+import { SignOut } from '../SignOut/SignOut';
+
+export function Dashboard() {
+  return (
+    <Container size={800} my={40}>
+      <Title ta="center" className={classes.title}>
+        Dashboard
+      </Title>
+			<Text ta="center">Welcome to your dashboard</Text>
+			<Text ta="center">More to come soon...</Text>
+      <TransactionList transactions={mockTransactionList}/>
+			<SignOut />
+      <ColorSchemeToggle />
+    </Container>
+  );
+}
