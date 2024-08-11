2e1a29da44fae8f7e4543a003e327d7d7bb116d9
cding21 <charlieding21@gmail.com>
2024-08-11 22:59:38 +1000

    feat: mobile responsiveness

--- src/frontend/components/Header/Header.tsx
+++ src/frontend/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Menu, Group, Center, Burger, Container, Button, Anchor } from '@mantine/core';
+import { Menu, Group, Center, Burger, Container, Button, Anchor, Accordion, Stack, Drawer } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconChevronDown, IconFileInfo, IconHelpHexagon } from '@tabler/icons-react';
 import classes from './Header.module.css';
@@ -19,6 +19,36 @@ const links = [
 export function Header() {
   const [opened, { toggle }] = useDisclosure(false);
   const dashboardURL = process.env.NEXT_PUBLIC_DASHBOARD_URL ?? 'http://localhost:3001';
+
+  const mobileItems = links.map((item) => (
+    <Accordion.Item key={item.link} value={item.link}>
+      <Accordion.Control>{item.label}</Accordion.Control>
+      <Accordion.Panel>
+        {item.links.map((subItem) => {
+          const { icon } = subItem;
+          return (
+            <Stack py="xs">
+              <Button fullWidth rightSection={icon} justify="space-between" variant="default">
+                {subItem.label}
+              </Button>
+            </Stack>
+          );
+        })}
+      </Accordion.Panel>
+    </Accordion.Item>
+  ));
+
+  mobileItems.push(
+    <Button 
+      component="a" 
+      href={dashboardURL} 
+      mt="md"
+      target="_blank" 
+      style={{ display: 'fixed', justifyContent: 'center' }}
+    >
+      Dashboard
+    </Button>
+  );
 
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
@@ -52,6 +82,11 @@ export function Header() {
 
   return (
     <header className={classes.header}>
+      <Drawer opened={opened} onClose={toggle}>
+        <Accordion variant="separated" radius="md">
+          {mobileItems}
+        </Accordion>
+      </Drawer>
       <Container size="md">
         <div className={classes.inner}>
           <Anchor component="a" href="/" underline="never">
