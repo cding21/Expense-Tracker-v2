77f41f410e5d369dd83b491e7e37be7d3f65d4ea
cding21 <charlieding21@gmail.com>
2024-08-15 17:40:01 +1000

    feat: new color scheme toggle button

--- src/dashboard/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/dashboard/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -5,18 +5,26 @@ import { FaMoon, FaSun } from 'react-icons/fa';
 import { BsNintendoSwitch } from 'react-icons/bs';
 
 export function ColorSchemeToggle() {
-  const { setColorScheme } = useMantineColorScheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const theme = {
+    light: {
+      icon: <FaSun />,
+      variant: 'outline',
+    },
+    dark: {
+      icon: <FaMoon />,
+      variant: 'filled',
+    },
+    auto: {
+      icon: <BsNintendoSwitch />,
+      variant: 'light',
+    },
+  };
 
   return (
     <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>
-        <FaSun />
-      </Button>
-      <Button onClick={() => setColorScheme('dark')}>
-        <FaMoon />
-      </Button>
-      <Button onClick={() => setColorScheme('auto')}>
-        <BsNintendoSwitch />
+      <Button variant={theme[colorScheme].variant} onClick={() => toggleColorScheme()} color="gray">
+        {theme[colorScheme].icon}
       </Button>
     </Group>
   );
